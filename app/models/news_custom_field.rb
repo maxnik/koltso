52e7702fe54a7b09c0c1ace086cfa11e3d18aceb
Max Nikolenko <maxim.nikolenko@gmail.com>
2010-06-11 00:52:14 +0300

    News can have Custom Fields now

---
+++ app/models/news_custom_field.rb
@@ -0,0 +1,5 @@
+class NewsCustomField < CustomField
+  def type_name
+    :label_news_plural
+  end
+end
--- lib/blog_news_custom_fields_patch.rb
+++ lib/blog_news_custom_fields_patch.rb
@@ -14,9 +14,39 @@ module BlogNewsCustomFieldsPatch
 
   module InstanceMethods
     def custom_fields_tabs_with_blog_news_tab
-      custom_fields_tabs_without_blog_tab << {:name => 'BlogCustomField', :partial => 'custom_fields/index', :label => :label_blog_plural}
+      custom_fields_tabs_without_blog_tab << {:name => 'BlogCustomField', :partial => 'custom_fields/index', :label => :label_blog_plural} << {:name => 'NewsCustomField', :partial => 'custom_fields/index', :label => :label_news_plural}
     end
   end
 end
 
 CustomFieldsHelper.send(:include, BlogNewsCustomFieldsPatch)
+
+# Code that follows is written after 'Adding a new method' section of
+# http://www.redmine.org/wiki/redmine/Plugin_Internals
+
+require_dependency 'news'
+
+module NewsPatch
+  def self.included(base)
+    # base.extend(ClassMethods)
+    # base.send(:include, InstanceMethods)
+
+    base.class_eval do
+      acts_as_customizable
+    end
+  end
+end
+
+News.send(:include, NewsPatch)
+
+require_dependency 'news_controller'
+
+module NewsControllerPatch
+  def self.included(base)
+    base.class_eval do
+      helper :custom_fields
+    end
+  end
+end
+
+NewsController.send(:include, NewsControllerPatch)
